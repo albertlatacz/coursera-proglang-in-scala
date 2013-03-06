78b0de824d20d35b182ba52712e6c4b499366a0c
Albert Latacz <albert.latacz@gmail.com>
2013-03-06 18:17:29 +0000

    removed unnecessary nested function and use pattern matching only in hw2 problem 1a

--- src/main/scala/HW2.scala
+++ src/main/scala/HW2.scala
@@ -10,50 +10,40 @@ object HW2 {
 
   def sameString(s1: String, s2: String) = s1 == s2
 
+  /**
+  1a. Write a function all_except_option, which takes a string and a string list. Return NONE if the string is not in the
+  list, else return SOME lst where lst is identical to the argument list except the string is not in it. You may assume
+  the string is in the list at most once. Use same_string, provided to you, to compare strings. Sample solution is
+  around 8 lines.*/
   def allExceptOption(excluded: String, list: Seq[String]): Option[Seq[String]] = list match {
     case Nil => None
-    case head :: tail if(excluded == head) => Some(tail)
+    case head :: tail if (excluded == head) => Some(tail)
     case head :: tail => allExceptOption(excluded, tail).map(head +: _)
   }
 
 
   // For problem 2
   abstract sealed class Suit
-
   case object Clubs extends Suit
-
   case object Diamonds extends Suit
-
   case object Hearts extends Suit
-
   case object Spades extends Suit
 
   abstract sealed class Rank
-
   case object Jack extends Rank
-
   case object Queen extends Rank
-
   case object King extends Rank
-
   case object Ace extends Rank
-
-  case class Num(num: Int) extends Rank
-
-  // you may assume that Num is always used with values 2, 3, ..., 10 though it will not really come up
+  case class Num(num: Int) extends Rank // you may assume that Num is always used with values 2, 3, ..., 10 though it will not really come up
 
   type Card = (Suit, Rank)
 
   abstract sealed class Color
-
   case object Red extends Color
-
   case object Black extends Color
 
   abstract sealed class Move
-
   case class Discard(card: Card)
-
   case object Draw
 
   class IllegalMove extends Exception
--- src/test/scala/HW2Spec.scala
+++ src/test/scala/HW2Spec.scala
@@ -5,6 +5,7 @@ class HW2Spec extends Specification {
 
   "allExceptOption returns Some(strings) if given string is in the list otherwise None" >> {
      allExceptOption("Hello", Seq("Hello", "big", "world!")) shouldEqual Some(Seq("big", "world!"))
+     allExceptOption("big", Seq("Hello", "big", "world!")) shouldEqual Some(Seq("Hello", "world!"))
      allExceptOption("not matching", Seq("Hello", "big", "world!")) shouldEqual None
   }
 
