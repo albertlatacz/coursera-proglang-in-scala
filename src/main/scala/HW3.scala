2aab561a082d8849eca66da1dcaf43f7f6b66585
Albert Latacz <albert.latacz@gmail.com>
2013-03-18 16:37:47 +0000

    added solution for hw3 problem 7

--- src/main/scala/HW3.scala
+++ src/main/scala/HW3.scala
@@ -8,6 +8,7 @@ object HW3 extends HW3Provided {
     */
   def onlyCapitals(strings: Seq[String]): Seq[String] = strings.filter(_.head.isUpper)
 
+
   /**
   2. Write a function longest_string1 that takes a string list and returns the longest string in the list. If the list
   is empty, return "". In the case of a tie, return the string closest to the beginning of the list. Use foldl,
@@ -17,6 +18,7 @@ object HW3 extends HW3Provided {
     strings.foldLeft("")((res, str) =>
       if (str.size > res.size) str
       else res)
+
 
   /**
   3. Write a function longest_string2 that is exactly like longest_string1 except in the case of ties it returns the
@@ -56,12 +58,29 @@ object HW3 extends HW3Provided {
     findLongestCapitalized(strings)
   }
 
+
   /**
   6. Write a function rev_string that takes a string and returns the string that is the same characters in reverse
   order. Use ML’s o operator, the library function rev for reversing lists, and two library functions in the String
   module. (Browse the module documentation to find the most useful functions.)
     */
   def revString(str: String): String = str.reverse
+
+
+  /**
+  7. Write a function first_answer of type (’a -> ’b option) -> ’a list -> ’b (notice the 2 argu- ments are curried).
+  The first argument should be applied to elements of the second argument in order until the first time it returns
+  SOME v for some v and then v is the result of the call to first_answer. If the first argument returns NONE for all
+  list elements, then first_answer should raise the exception NoAnswer. Hints: Sample solution is 5 lines and does
+  nothing fancy.
+    */
+  def firstAnswer[A, B](fn: (A) => Option[B])(items: Seq[A]): B = items match {
+    case Nil => throw new NoAnswer
+    case head :: tail => fn(head) match {
+      case None => firstAnswer(fn)(tail)
+      case Some(value) => value
+    }
+  }
 
 }
 
--- src/test/scala/HW3Spec.scala
+++ src/test/scala/HW3Spec.scala
@@ -36,4 +36,11 @@ class HW3Spec extends Specification {
     revString("Some String") shouldEqual "gnirtS emoS"
   }
 
+  "firstAnswer returns correct result" >> {
+    def evenOption(value: Integer) = if (value % 2 == 0) Some(value) else None
+    firstAnswer(evenOption)(Seq(1, 3, 4, 5, 6)) shouldEqual 4
+    firstAnswer(evenOption)(Seq(1, 3, 5, 7, 9)) should throwA[NoAnswer]
+  }
+
+
 }
